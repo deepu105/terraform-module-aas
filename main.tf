29111ace63396f0322b959900a427c1745e3fbf5
deepu105 <d4udts@gmail.com>
2019-08-13 16:17:57 +0200

    init

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "azurerm" {
+  version         = "=1.24.0"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+}
+
+# This creates the plan that the service use
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+# This creates the service definition
+resource "azurerm_app_service" "main" {
+  name                = "${var.prefix}-appservice"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|${var.docker_image}:${var.docker_image_tag}"
+    always_on        = true
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+
+    # These are app specific environment variables go here
+    "SPRING_PROFILES_ACTIVE"     = "dev"
+  }
+}
